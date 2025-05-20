b5575197bec820ebe9be10232f21f205f5294c6d
hocpt <hboy22vn@gmail.com>
2025-05-20 22:27:53 +0700

    Delete .history directory

--- .history/app/generate_key_20250504205045.py
+++
@@ -1,4 +0,0 @@
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-print("Your new encryption key (add this to .env as ENCRYPTION_KEY):")
-print(key.decode()) # In key dưới dạng text để dễ copy
